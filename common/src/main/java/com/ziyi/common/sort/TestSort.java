3beec3eec07bf076aec084e471bab753b0db8c47
zhangyi <1027230413@qq.com>
2021-04-14 20:38:51 +0800

    工厂+单例模式 实现排序算法：选择排序

--- common/src/main/java/com/ziyi/common/sort/TestSort.java
+++ common/src/main/java/com/ziyi/common/sort/TestSort.java
@@ -1,14 +1,17 @@
 package com.ziyi.common.sort;
 
 /**
- * auther:jurzis
+ * auther:jurzis 排序算法，从小到大排列
  * date: 2021/4/14 16:26
  */
 public class TestSort {
 
     public static void main(String[] args) {
         int [] arg = {3,2,68,2,5,6,9,0,12,54};
-        ISort sort = new BubbleSort();
+        //冒泡排序
+        //ISort sort = new BubbleSort();
+        //选择排序
+        ISort sort = new SelectSort();
         SortFactory sortFactory = SortFactory.getInstance(sort);
         int [] result = sortFactory.sort(arg);
         print(result);
@@ -16,7 +19,8 @@ public class TestSort {
 
     private static void print(int[] arg){
         for (int a : arg) {
-            System.out.println(a);
+            System.out.print(a);
+            System.out.print(",");
         }
     }
 
