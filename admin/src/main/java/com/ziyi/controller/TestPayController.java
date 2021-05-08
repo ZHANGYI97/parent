5ed62243b8982441a99d1e7198f53876e808c411
zhangyi <1027230413@qq.com>
2021-05-08 10:22:07 +0800

    框架搭建-支付宝接入 工具类编写

---
+++ admin/src/main/java/com/ziyi/controller/TestPayController.java
@@ -0,0 +1,15 @@
+package com.ziyi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@RestController
+@RequestMapping("/pay")
+public class TestPayController {
+
+
+
+}
